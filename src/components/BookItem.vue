b3fc61cc8f7b3434a432fc6cd920cd61b2e85238
samvue <74465878+samvue@users.noreply.github.com>
2020-11-14 18:22:48 +0000

    Add files via upload

---
+++ src/components/BookItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <li>{{book.title}}:{{book.author}}</li>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: BookItem,
+        props: ['book']
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
