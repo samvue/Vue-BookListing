8a0360a3c15fa0a1e3a9031f53d5d209c1a4f29f
samvue <74465878+samvue@users.noreply.github.com>
2020-11-14 18:53:55 +0000

    Add files via upload

---
+++ src/components/BookForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <form class="" action="#" method="post" @submit="bookSubmit">
+      <input type="text" name="title" value="" placeholder="Book Title" />
+      <input type="text" name="author" value="" placeholder="Book Author" />
+      <button type="submit" name="button">Add Book</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BookForm",
+  props: ["books"],
+  data() {
+    return {
+      bookTitle: "",
+      bookAuthor: "",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
